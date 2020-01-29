dd9367cb702656a098c2e0b6b8653297458de9b2
Sem Shekhovtsov <semshehovtsov@gmail.com>
2020-01-30 03:36:25 +0800

    implementing second task for page content scrolling

---
+++ src/app/Tasks/second-task/second-task.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SecondTaskComponent } from './second-task/second-task.component';
+
+
+
+@NgModule({
+  declarations: [SecondTaskComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class SecondTaskModule { }
